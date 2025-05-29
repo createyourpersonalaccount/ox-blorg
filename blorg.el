cc9e5d3b0c85e7e0efe56f43178eb6a526f816b7
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2025-05-29 17:38:00 -0400

    add license notice

--- blorg.el
+++ blorg.el
@@ -1,10 +1,26 @@
 ;;; blorg.el --- Your blog in Org -*- lexical-binding: t; -*-
 
+;; blorg.el, Your blog in Org
+;; Copyright (C) 2025  Nikolaos Chatzikonstantinou
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;; Copyright (C) 2025 Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
 ;; Author: Nikolaos Chatzikonstantinou
 ;; URL: https://github.com/createyourpersonalaccount/blorg
 ;; Created: 2025
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Keywords: outlines org blog
 ;; Package-Requires: ((emacs "30.1"))
 
